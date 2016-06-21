fa23ec10ab4a90ae95fb277acff0ef14d553a86e
Loretapiap <loretapiap@gmail.com>
2016-06-21 13:24:26 -0400

    Ejercicios del 1 al 10 resueltos, restante: 6

--- Ejercicio10- Cantidad de digitos/Problema/main.js
+++ Ejercicio10- Cantidad de digitos/Problema/main.js
@@ -1,4 +1,12 @@
-// Escribe aquí tu codigo Javascript
+function numero(){
+	var n1 = prompt("Ingrese un numero");
+		while (!/^(\+|-)?[0-9]\d*(\.\d*)?$/.test(n1)){
+		n1 = prompt("Sólo ingrese números por favor.");
+		}
+	var resultado = n1.length;
+
+	alert(resultado);
+}
 
 
 

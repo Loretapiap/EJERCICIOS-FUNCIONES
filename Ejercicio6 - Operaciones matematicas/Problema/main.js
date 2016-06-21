4138c11784de6fbffd98108f11ede58b6d0668cd
Loretapiap <loretapiap@gmail.com>
2016-06-21 13:40:13 -0400

    Ejercicios terminados

--- Ejercicio6 - Operaciones matematicas/Problema/main.js
+++ Ejercicio6 - Operaciones matematicas/Problema/main.js
@@ -1,4 +1,6 @@
-// Lo dejare para el final (: 
-
-
-
+function calcula(operacion){
+    var operando1 = document.calc.operando1.value
+    var operando2 = document.calc.operando2.value 
+    var result = eval(operando1 + operacion + operando2)
+    document.calc.resultado.value = result
+}
